fa7f083a8a839375c5742b0bf37fcbddad9f81db
Efereyan Karen Simisola <thedevkaren@gmail.com>
2020-11-13 13:04:12 +0100

    Require graphql and express-graphql packages for querying the Graphql way
    Signed-off-by: Efereyan Karen Simisola <thedevkaren@gmail.com>

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require('express');
 
+const { graphqlHTTP } = require('express-graphql');
 
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull
+} = require('graphql')
 
 app.listen(5000, () => console.log('Server Running'))
